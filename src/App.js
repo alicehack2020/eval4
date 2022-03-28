c4f119b1cebc53b3df84469b91430e035df1007e
Mangesh2042 <mangesh2042@gmail.com>
2022-03-28 21:35:35 +0530

    step1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Menu/Login';
+import Register from './Menu/Register';
+import Product from './Menu/Product';
+import Home from './Menu/Home';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="../Menu/Login">Login</Link>
+          </li>
+          <li>
+            <Link to="../Menu/Register">Register</Link>
+          </li>
+          <li>
+            <Link to="../Menu/Product">Register</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Login">
+            <Login />
+          </Route>
+
+          <Route path="/Register">
+            <Register />
+          </Route>
+
+          <Route path="/Product">
+            <Product />
+          </Route>
+
+
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+        <div>Home</div>
+        <div>Login</div>
+        <div>Register</div>
+        <div>Product</div>
+    </div>
+  )
+}
+
+export default Menu
